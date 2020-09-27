0591cc3cfd847f416367192d2c453b54fcb0af10
Henry Eaton <henaree@gmail.com>
2020-09-27 19:06:51 +0100

    Update ip_address.py

--- scripts/ip_address.py
+++ scripts/ip_address.py
@@ -1,18 +1,46 @@
 import I2C_LCD_driver
-import socket
-import fcntl
-import struct
+from subprocess import Popen, PIPE
+from time import sleep
+from datetime import datetime
 
+#initilise LCD screen
 mylcd = I2C_LCD_driver.lcd()
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+# looking for an active Ethernet or WiFi device
+def find_interface():
+    find_device = "ip addr show"
+    interface_parse = run_cmd(find_device)
+    for line in interface_parse.splitlines():
+        if "state UP" in line:
+            dev_name = line.split(':')[1]
+    return dev_name
 
-mylcd.lcd_display_string("IP Address:", 1)
+# find an active IP on the first LIVE network device
+def parse_ip():
+    find_ip = "ip addr show %s" % interface
+    find_ip = "ip addr show %s" % interface
+    ip_parse = run_cmd(find_ip)
+    for line in ip_parse.splitlines():
+        if "inet " in line:
+            ip = line.split(' ')[5]
+            ip = ip.split('/')[0]
+    return ip
 
-mylcd.lcd_display_string(get_ip_address('wlan0'), 2)
+# run unix shell command, return as ASCII
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output.decode('ascii')
+
+# before we start the main loop - detect active network device and ip address
+sleep(2)
+interface = find_interface()
+ip_address = parse_ip()
+
+
+while True:
+
+        mylcd.lcd_display_string("IP Address:", 1)
+        mylcd.lcd_display_string("{}".format(ip_address), 2)
+
+        sleep(2)
